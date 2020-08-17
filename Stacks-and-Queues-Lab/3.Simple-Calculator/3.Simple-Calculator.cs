41b998fe4eccf72cdab6566f3189bcad23163fb7
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-17 13:57:01 +0300

    Create 3.Simple-Calculator.cs
    Create a simple calculator that can evaluate simple expressions with only addition and subtraction. There will not be any parentheses.
    This is the solution.

---
+++ Stacks-and-Queues-Lab/3.Simple-Calculator/3.Simple-Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Stack<string> stack = new Stack<string>(input
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Reverse());
+
+        while (stack.Count > 1)
+        {
+            int operanad1 = int.Parse(stack.Pop());
+            string sign = stack.Pop();
+            int operand2 = int.Parse(stack.Pop());
+
+            switch (sign)
+            {
+                case "+":
+                    stack.Push((operanad1 + operand2).ToString());
+                    break;
+                case "-":
+                    stack.Push((operanad1 - operand2).ToString());
+                    break;
+                default:
+                    throw new ArgumentException("Unknown operator");
+            }
+        }
+        Console.WriteLine(stack.Pop());
+    }
+}
+
