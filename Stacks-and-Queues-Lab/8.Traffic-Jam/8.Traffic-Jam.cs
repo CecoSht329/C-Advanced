375aca851c180776a36c04974f95bafc07968f4d
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-17 16:26:09 +0300

    Create 8.Traffic-Jam.cs
    Create a program that simulates the queue that forms during a traffic jam. During a traffic jam only N cars can pass the crossroads when the light goes green. Then the program reads the vehicles that arrive one by one and adds them to the queue. When the light goes green N number of cars pass the crossroads and for each a message "{car} passed!" is displayed. When the "end" command is given, terminate the program and display a message with the total number of cars that passed the crossroads.

---
+++ Stacks-and-Queues-Lab/8.Traffic-Jam/8.Traffic-Jam.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+        string token = Console.ReadLine();
+        Queue<string> cars = new Queue<string>();
+        int counter = 0;
+
+        while (token.ToLower() != "end")
+        {
+            if (token.ToLower() != "green")
+            {
+                cars.Enqueue(token);
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    string car;
+                    if (cars.TryDequeue(out car))
+                    {
+                        Console.WriteLine($"{car} passed!");
+                        counter++;
+                    }
+                }
+            }
+            token = Console.ReadLine();
+        }
+        Console.WriteLine($"{counter} cars passed the crossroads.");
+    }
+}
+
