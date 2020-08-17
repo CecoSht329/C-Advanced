82b0b52d3ee69c3407c274aa24d29a153a3d7dd9
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-17 15:21:13 +0300

    Create 7.Hot-Potato.cs

---
+++ Stacks-and-Queues-Lab/7.Hot-Potato/7.Hot-Potato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int tosses = int.Parse(Console.ReadLine());
+
+        Queue<string> kids = new Queue<string>(input
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries));
+        while (kids.Count > 1)
+        {
+            for (int i = 1; i < tosses; i++)
+            {
+                string kid = kids.Dequeue();
+                kids.Enqueue(kid);
+            }
+            Console.WriteLine($"Removed {kids.Dequeue()}");
+        }
+        Console.WriteLine($"Last is {kids.Dequeue()}");
+    }
+}
+
