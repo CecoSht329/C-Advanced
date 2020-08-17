8cd0c7b1171831ef0cce1ff880def4589cedc291
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-17 14:35:32 +0300

    Create 4.Matching-Brackets.cs
    We are given an arithmetic expression with brackets. Scan through the string and extract each sub-expression.
    Print the result back at the terminal.

---
+++ Stacks-and-Queues-Lab/4.Matching-Brackets/4.Matching-Brackets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] inputToArray = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                stack.Push(i);
+            }
+            else if (input[i] == ')')
+            {
+                int start = stack.Pop();
+                Console.WriteLine(input.Substring(start, i - start + 1));
+            }
+        }
+    }
+}
+
