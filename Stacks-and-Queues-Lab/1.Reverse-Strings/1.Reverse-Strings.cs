83555e05c0275c5bd37bbe897f0ff8e639d8a165
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-17 15:43:50 +0300

    Update 1.Reverse-Strings.cs

--- Stacks-and-Queues-Lab/1.Reverse-Strings/1.Reverse-Strings.cs
+++ Stacks-and-Queues-Lab/1.Reverse-Strings/1.Reverse-Strings.cs
@@ -7,16 +7,23 @@ class Program
     {
         string input = Console.ReadLine();
 
-        Stack<char> stack = new Stack<char>();
+        Stack<char> stack = new Stack<char>(input);
+        char symbol;
+        while (stack.TryPop(out symbol) == true)
+        {
+            Console.Write(symbol);
+        }
 
-        for (int i = 0; i < input.Length; i++)
-        {
-            stack.Push(input[i]);
-        }
-        while (stack.Count > 0)
-        {
-            Console.Write(stack.Pop());
-        }
+        //for (int i = 0; i < input.Length; i++)
+        //{
+        //    stack.Push(input[i]);
+        //}
+        //while (stack.Count > 0)
+        //{
+        //    Console.Write(stack.Pop());
+        //}
+
+
     }
 }
 
