41c0b25b201db17fd223a2a706b3d87b704fa746
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-18 12:47:50 +0300

    Create 6.Supermarket.cs
    Super market simulation using a queue

---
+++ Stacks-and-Queues-Lab/6.Supermarket/6.Supermarket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> queueInfrontOfSuperMarket = new Queue<string>();
+
+        string input = string.Empty;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            
+            if (input != "Paid")
+            {
+                queueInfrontOfSuperMarket.Enqueue(input);
+            }
+            else
+            {
+                while (queueInfrontOfSuperMarket.Count > 0)
+                {
+                    Console.WriteLine(queueInfrontOfSuperMarket.Dequeue());
+                }
+            }
+        }
+        Console.WriteLine($"{queueInfrontOfSuperMarket.Count} people remaining.");
+    }
+}
+
