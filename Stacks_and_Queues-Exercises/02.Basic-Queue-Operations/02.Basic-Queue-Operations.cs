ccaeebf65ebe426a9d475dc5143f9c6faf183b71
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-18 15:47:57 +0300

    Create 02.Basic-Queue-Operations.cs

---
+++ Stacks_and_Queues-Exercises/02.Basic-Queue-Operations/02.Basic-Queue-Operations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int numberOfElementsToEnqueue = numbers[0];
+        int numberOfElementsToDequeue = numbers[1];
+        int numberToLookFor = numbers[2];
+        int[] numbersToGoInQueue = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> queueNumbers = new Queue<int>();
+        for (int i = 0; i < numberOfElementsToEnqueue; i++)
+        {
+            queueNumbers.Enqueue(numbersToGoInQueue[i]);
+        }
+        for (int i = 0; i < numberOfElementsToDequeue; i++)
+        {
+            queueNumbers.Dequeue();
+        }
+        if (queueNumbers.Contains(numberToLookFor))
+        {
+            Console.WriteLine("true");
+        }
+        else if (!queueNumbers.Contains(numberToLookFor) && queueNumbers.Count > 0)
+        {
+            int smallestNumber = int.MaxValue;
+            foreach (int number in queueNumbers)
+            {
+                if (number < smallestNumber)
+                {
+                    smallestNumber = number;
+                }
+            }
+            Console.WriteLine(smallestNumber);
+        }
+        else if (queueNumbers.Count == 0)
+        {
+            Console.WriteLine(queueNumbers.Count);
+        }
+    }
+}
+
