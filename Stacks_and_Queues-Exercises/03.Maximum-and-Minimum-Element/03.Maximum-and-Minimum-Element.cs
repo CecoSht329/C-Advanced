92ae4ebaaa8b6748d83563506bfb267acfcd1973
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-18 16:22:44 +0300

    Create 03.Maximum-and-Minimum-Element.cs

---
+++ Stacks_and_Queues-Exercises/03.Maximum-and-Minimum-Element/03.Maximum-and-Minimum-Element.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int queries = int.Parse(Console.ReadLine());
+
+        Stack<int> stackOfNums = new Stack<int>();
+
+        for (int i = 0; i < queries; i++)
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            if (input.StartsWith("1"))
+            {
+                int element = int.Parse(tokens[1]);
+                stackOfNums.Push(element);
+            }
+            else if (input.StartsWith("2"))
+            {
+                if (stackOfNums.Count > 0)
+                {
+                    stackOfNums.Pop();
+                }
+            }
+            else if (input.StartsWith("3"))
+            {
+                int maxElement = int.MinValue;
+                foreach (int number in stackOfNums)
+                {
+                    if (number > maxElement)
+                    {
+                        maxElement = number;
+                    }
+                }
+                Console.WriteLine(maxElement);
+            }
+            else if (input.StartsWith("4"))
+            {
+                int minElement = int.MaxValue;
+                foreach (int number in stackOfNums)
+                {
+                    if (number < minElement)
+                    {
+                        minElement = number;
+                    }
+                }
+                Console.WriteLine(minElement);
+            }
+        }
+        Console.WriteLine(string.Join(", ", stackOfNums));
+    }
+}
+
