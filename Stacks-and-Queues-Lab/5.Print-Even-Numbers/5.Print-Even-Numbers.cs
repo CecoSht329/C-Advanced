90d130419c3f54ab3759f5ce63b0426f07dee7cc
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-18 12:36:02 +0300

    Create 5.Print-Even-Numbers.cs
    Print even numbers from array using a queue

---
+++ Stacks-and-Queues-Lab/5.Print-Even-Numbers/5.Print-Even-Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> queue = new Queue<int>(numbers.Where(x => x % 2 == 0));
+
+        Console.WriteLine(string.Join(", ", queue));
+    }
+}
+
