91aeb4e9c3383d6d971c5057b00b003c3a831bec
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-19 15:00:10 +0300

    Create 7.Truck-Tour.cs
    Simulation of a truck with endless fueltank .Find the point where if the truck starts its tour it can go on forever.

---
+++ Stacks_and_Queues-Exercises/7.Truck-Tour/7.Truck-Tour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Queue<int[]> pumps = new Queue<int[]>();
+        FillQueue(n, pumps);
+
+        int counter = 0;
+
+        while (true)
+        {
+            int fuelAmount = 0;
+            bool foundPoint = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentPump = pumps.Dequeue();
+
+                fuelAmount += currentPump[0];
+
+                if (fuelAmount < currentPump[1])
+                {
+                    foundPoint = false;
+                }
+
+                fuelAmount -= currentPump[1];
+                pumps.Enqueue(currentPump);
+            }
+            if (foundPoint)
+            {
+                break;
+            }
+            counter++;
+            pumps.Enqueue(pumps.Dequeue());//this makes the circle simulation in the Queue
+        }
+        Console.WriteLine(counter);
+    }
+
+    private static void FillQueue(int n, Queue<int[]> pumps)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int[] currentPump = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            pumps.Enqueue(currentPump);
+        }
+    }
+}
+
