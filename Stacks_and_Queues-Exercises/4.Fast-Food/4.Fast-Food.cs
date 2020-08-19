de79445e031961da655e7186ee405bf9ab952e0e
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-19 09:33:33 +0300

    Create 4.Fast-Food.cs
    Fast food restaurant queue simulation

---
+++ Stacks_and_Queues-Exercises/4.Fast-Food/4.Fast-Food.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int quantityFood = int.Parse(Console.ReadLine());
+        int[] orders = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> queue = new Queue<int>(orders);
+        int biggestOrder = int.MinValue;
+        foreach (int order in orders)
+        {
+            if (order > biggestOrder)
+            {
+                biggestOrder = order;
+            }
+            if (quantityFood >= order)
+            {
+                quantityFood -= order;
+                queue.Dequeue();
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine(biggestOrder);
+        if (queue.Count == 0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+        }
+    }
+}
+
