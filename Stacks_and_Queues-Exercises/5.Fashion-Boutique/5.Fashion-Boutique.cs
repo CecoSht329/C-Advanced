c5726812ffa25a49b45bfd437bf5806e95cbf5b7
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-19 12:55:03 +0300

    Create 5.Fashion-Boutique.cs
    Clothes shop simulation

---
+++ Stacks_and_Queues-Exercises/5.Fashion-Boutique/5.Fashion-Boutique.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] clothesArr = Console.ReadLine()
+             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+             .Select(int.Parse)
+             .ToArray();
+        Stack<int> boxes = new Stack<int>(clothesArr);
+        int rackCapacity = int.Parse(Console.ReadLine());
+        int rackCount = 1;
+        int currentRackCapacity = rackCapacity;
+
+        while (boxes.Count > 0)
+        {
+            int currentClothes = boxes.Peek();
+
+            if (currentRackCapacity >= currentClothes)
+            {
+                currentRackCapacity -= currentClothes;
+                boxes.Pop();
+            }
+            else
+            {
+                rackCount++;
+                currentRackCapacity = rackCapacity;
+            }
+        }
+        Console.WriteLine(rackCount);
+    }
+}
+
