804bf0cbde218fe185c25de7ebf9ff3cd3ee6a3e
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-19 13:39:35 +0300

    Create 6.Songs-Queue.cs
    mp3 player simulation

---
+++ Stacks_and_Queues-Exercises/06.Songs-Queue/6.Songs-Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] songsInput = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+        Queue<string> songs = new Queue<string>(songsInput);
+
+        while (songs.Any())
+        {
+            string commandInput = Console.ReadLine();
+            if (commandInput.StartsWith("Play"))
+            {
+                songs.Dequeue();
+            }
+            else if (commandInput.StartsWith("Add"))
+            {
+                string song = commandInput.Substring(4);
+                if (!songs.Contains(song))
+                {
+                    songs.Enqueue(song);
+                }
+                else
+                {
+                    Console.WriteLine($"{song} is already contained!");
+                }
+            }
+            else if (commandInput.StartsWith("Show"))
+            {
+                Console.WriteLine(string.Join(", ", songs));
+            }
+        }
+        Console.WriteLine("No more songs!");
+    }
+}
+
