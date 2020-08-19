16cb4522bc35fbb2e3299929ea58b7e6a64f2c4b
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-19 16:25:31 +0300

    Create 8.Balanced-Parenthesis.cs
    Check if brackets are balanced

---
+++ Stacks_and_Queues-Exercises/8.Balanced-Parenthesis/8.Balanced-Parenthesis.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+        Stack<char> openParentheses = new Stack<char>();
+
+        bool isBalanced = true;
+        foreach (char symbol in expression)
+        {
+            if (symbol == '(' || symbol == '[' || symbol == '{')
+            {
+                openParentheses.Push(symbol);
+            }
+            else
+            {
+                if (!openParentheses.Any())
+                {
+                    isBalanced = false;
+                    break;
+                }
+
+                char currentOpenBracket = openParentheses.Pop();
+
+                bool isRoundBalanced = currentOpenBracket == '(' && symbol == ')';
+                bool isCurlyBalanced = currentOpenBracket == '{' && symbol == '}';
+                bool isSquareBalanced = currentOpenBracket == '[' && symbol == ']';
+
+                if (!isRoundBalanced && !isCurlyBalanced && !isSquareBalanced)
+                {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+        if (isBalanced)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
+
