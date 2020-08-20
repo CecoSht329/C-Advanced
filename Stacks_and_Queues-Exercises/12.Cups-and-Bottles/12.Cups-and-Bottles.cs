62e6bd598d2a8a0da3073f15c4bca0731c2d3220
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-20 15:41:32 +0300

    Create 12.Cups-and-Bottles.cs
    Simulation of pouring water in cups from bottles.

---
+++ Stacks_and_Queues-Exercises/12.Cups-and-Bottles/12.Cups-and-Bottles.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] cupsSequence = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] bottlesSequence = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Queue<int> cups = new Queue<int>(cupsSequence);
+        Stack<int> bottles = new Stack<int>(bottlesSequence);
+
+        int wastedWater = 0;
+        while (cups.Any() && bottles.Any())
+        {
+            int bottle = bottles.Peek();
+            int cup = cups.Peek();
+            if (bottle - cup >= 0)
+            {
+                bottles.Pop();
+                cups.Dequeue();
+                wastedWater += bottle - cup;
+            }
+            else
+            {
+                while (cup - bottle > 0)
+                {
+                    cup -= bottle;
+                    bottles.Pop();
+                    bottle = bottles.Peek();
+                }
+                cups.Dequeue();
+                wastedWater += bottle - cup;
+                bottles.Pop();
+            }
+        }
+        if (!cups.Any())
+        {
+            Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+        }
+        else if (!bottles.Any())
+        {
+            Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+        }
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+    }
+}
+
