c3a90cda24cf226fac28d8fe3e69750020915168
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-21 11:51:39 +0300

    Create 4.Symbol-in-Matrix.cs
    Find coordiantes of a symbol in matrix

---
+++ MultidimentionalArrays-LAB/4.Symbol-in-Matrix/4.Symbol-in-Matrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int rowsAndCols = int.Parse(Console.ReadLine());
+        int rows = rowsAndCols;
+        int cols = rowsAndCols;
+
+        char[,] matrix = new char[rows, cols];
+        for (int row = 0; row < rows; row++)
+        {
+            string currentRow = Console.ReadLine();
+            char[] currentRowToArray = currentRow.ToCharArray();
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = currentRowToArray[col];
+            }
+        }
+
+        char symbolToSearchFor = char.Parse(Console.ReadLine());
+
+        int[] coordinates = new int[2];
+        bool isFound = false;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                char currentSymbol = matrix[row, col];
+                if (symbolToSearchFor == currentSymbol)
+                {
+                    coordinates[0] = row;
+                    coordinates[1] = col;
+                    isFound = true;
+                    break;
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"({string.Join(", ", coordinates)})");
+                break;
+            }
+        }
+        if (!isFound)
+        {
+            Console.WriteLine($"{symbolToSearchFor} does not occur in the matrix");
+        }
+    }
+}
+
