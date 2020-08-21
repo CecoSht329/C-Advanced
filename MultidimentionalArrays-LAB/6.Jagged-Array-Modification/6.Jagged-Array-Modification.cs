746eaf06a6ed02696c8d07708f2c5f2e0d33a334
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-21 14:41:58 +0300

    Create 6.Jagged-Array-Modification.cs
    Jagged Array adding and subtracting numbers from elements in array

---
+++ MultidimentionalArrays-LAB/6.Jagged-Array-Modification/6.Jagged-Array-Modification.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        int[][] array = new int[rows][];
+
+        for (int row = 0; row < rows; row++)
+        {
+            int[] currentRow = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            array[row] = currentRow;
+        }
+
+        string input = "";
+        while ((input = Console.ReadLine()).ToLower() != "end")
+        {
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = tokens[0];
+            int row = int.Parse(tokens[1]);
+            int col = int.Parse(tokens[2]);
+            int value = int.Parse(tokens[3]);
+
+            if (row < 0
+                || row >= rows
+                || col < 0
+                || col >= array[row].Length)
+            {
+                Console.WriteLine("Invalid coordinates");
+                continue;
+            }
+
+            if (command == "Add")
+            {
+                array[row][col] += value;
+            }
+            else if (command == "Subtract")
+            {
+                array[row][col] -= value;
+            }
+        }
+        foreach (int[] currentRow in array)
+        {
+            Console.WriteLine(string.Join(" ", currentRow));
+        }
+    }
+}
+
