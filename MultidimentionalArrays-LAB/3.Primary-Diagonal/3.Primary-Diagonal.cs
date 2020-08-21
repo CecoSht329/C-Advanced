e4b672aff70b213a10f141feaa2bdb8fa4a4d0c1
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-21 10:48:56 +0300

    Create 3.Primary-Diagonal.cs
    Sum of primary diagonal in square matrix

---
+++ MultidimentionalArrays-LAB/3.Primary-Diagonal/3.Primary-Diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int numberOfRowsAndCols = int.Parse(Console.ReadLine());
+
+        int rows = numberOfRowsAndCols;
+        int cols = numberOfRowsAndCols;
+
+        //int[,] matrix = new int[rows, cols];
+        int sumOfPrimaryDiagonal = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            int[] currentRow = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+            sumOfPrimaryDiagonal += currentRow[row];
+        }
+        Console.WriteLine(sumOfPrimaryDiagonal);
+    }
+}
+
