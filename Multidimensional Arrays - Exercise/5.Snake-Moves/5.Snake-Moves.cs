afdb25edda1b83210db4e80f82319a9f3b931d30
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-23 14:41:46 +0300

    Create 5.Snake-Moves.cs
    Simulate snake moves with a string

---
+++ Multidimensional Arrays - Exercise/5.Snake-Moves/5.Snake-Moves.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        int[] dimentions = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToArray();
+        string snake = Console.ReadLine();
+
+        int rows = dimentions[0];
+        int cols = dimentions[1];
+        char[,] matrix = new char[rows, cols];
+
+        int counter = 0;
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = snake[counter];
+                counter++;
+                if (counter == snake.Length)
+                {
+                    counter = 0;
+                }
+            }
+        }
+        Console.WriteLine();
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            if (row % 2 == 0)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+            }
+            else
+            {
+                for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
