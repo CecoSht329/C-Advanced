cbafd55407c76d5b5dd428e6e8a009a0ef8906c0
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-25 11:36:23 +0300

    Create 1.Count-Same-Values-in-Array.cs
    Count decimal nums in array

---
+++ Sets-And-Dictionaries-LAB/1.Count-Same-Values-in-Array/1.Count-Same-Values-in-Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numberCounter = new Dictionary<double, int>();
+
+        var arrayOfDecimalNumbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .ToArray();
+        foreach (var number in arrayOfDecimalNumbers)
+        {
+            if (!numberCounter.ContainsKey(number))
+            {
+                numberCounter.Add(number, 0);
+            }
+            numberCounter[number]++;
+        }
+        foreach (var kvp in numberCounter)
+        {
+            Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+        }
+
+    }
+}
+
