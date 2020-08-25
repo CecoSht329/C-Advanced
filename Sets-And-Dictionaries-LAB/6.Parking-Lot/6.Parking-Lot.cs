683a8a53076c59700c1dcbb5e29989435136664d
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-25 16:52:41 +0300

    Create 6.Parking-Lot.cs

---
+++ Sets-And-Dictionaries-LAB/6.Parking-Lot/6.Parking-Lot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var carNumbers = new HashSet<string>();
+        var input = "";
+        while ((input = Console.ReadLine()).ToLower() != "end")
+        {
+            var tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            var direction = tokens[0];
+            var liscencePlate = tokens[1];
+
+            if (direction.ToLower() == "in")
+            {
+                carNumbers.Add(liscencePlate);
+            }
+            else if (direction.ToLower() == "out")
+            {
+                if (carNumbers.Contains(liscencePlate))
+                {
+                    carNumbers.Remove(liscencePlate);
+                }
+            }
+        }
+        if (carNumbers.Count > 0)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, carNumbers));
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
+
