7a26fd565fef8a944d31c2b583e5972aa41c09c2
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-25 16:42:25 +0300

    Create 5.Record-Unique-Names.cs

---
+++ Sets-And-Dictionaries-LAB/5.Record-Unique-Names/5.Record-Unique-Names.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int namesCount = int.Parse(Console.ReadLine());
+        var uniqueNames = new HashSet<string>();
+        for (int i = 0; i < namesCount; i++)
+        {
+            string name = Console.ReadLine();
+            uniqueNames.Add(name);
+        }
+        Console.WriteLine();
+        Console.WriteLine(string.Join(Environment.NewLine, uniqueNames));
+    }
+}
+
