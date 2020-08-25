57556405c249e80cf5f955c1d6cada4280a4c19b
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-08-25 11:01:36 +0300

    Add files via upload

---
+++ Sets_and_Dictionaries_Advanced-Lab/1.Count-Same-Values-in-Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
