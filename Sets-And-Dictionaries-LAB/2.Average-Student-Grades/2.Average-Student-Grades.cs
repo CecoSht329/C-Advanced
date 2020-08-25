d0997a63b419f73b4f00e3de4fc717b3940a9b00
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-25 14:24:19 +0300

    Create 2.Average-Student-Grades.cs

---
+++ Sets-And-Dictionaries-LAB/2.Average-Student-Grades/2.Average-Student-Grades.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var grades = new Dictionary<string, List<decimal>>();
+
+        int studentCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < studentCount; i++)
+        {
+            string[] inputInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = inputInfo[0];
+            decimal grade = decimal.Parse(inputInfo[1]);
+
+            if (!grades.ContainsKey(name))
+            {
+                grades.Add(name, new List<decimal>());
+            }
+            grades[name].Add(grade);
+        }
+
+        foreach (var keyValuePair in grades)
+        {
+            Console.Write($"{keyValuePair.Key} -> ");
+            foreach (var item in keyValuePair.Value)
+            {
+                Console.Write($"{item:F2} ");
+            }
+            Console.WriteLine($"(avg: {keyValuePair.Value.Average():F2})");
+        }
+    }
+}
+
