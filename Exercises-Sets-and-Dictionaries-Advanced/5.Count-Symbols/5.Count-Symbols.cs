f92a7121eae51dccfc3136c41833722480866633
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-26 11:53:17 +0300

    Create 5.Count-Symbols.cs

---
+++ Exercises-Sets-and-Dictionaries-Advanced/5.Count-Symbols/5.Count-Symbols.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var occurences = new SortedDictionary<char, int>();
+
+        var text = Console.ReadLine();
+        foreach (var symbol in text)
+        {
+            if (!occurences.ContainsKey(symbol))
+            {
+                occurences.Add(symbol, 0);
+            }
+            occurences[symbol]++;
+        }
+        foreach (var kvp in occurences)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+        }
+    }
+}
+
