2ba5924df4b353bc285c94f5c984eafe4eae5a8a
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-26 11:06:27 +0300

    Create 3.Periodic-Table.cs

---
+++ Exercises-Sets-and-Dictionaries-Advanced/3.Periodic-Table/3.Periodic-Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var uniuniqueElements = new SortedSet<string>();
+
+        int linesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < linesCount; i++)
+        {
+            var compound = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var element in compound)
+            {
+                uniuniqueElements.Add(element);
+            }
+        }
+        Console.WriteLine(string.Join(" ", uniuniqueElements));
+    }
+}
+
