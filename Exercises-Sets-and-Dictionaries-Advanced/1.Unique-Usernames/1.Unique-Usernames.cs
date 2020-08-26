291703e2df406760d17b60f14d96944001d52460
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-26 10:31:46 +0300

    Create 1.Unique-Usernames.cs

---
+++ Exercises-Sets-and-Dictionaries-Advanced/1.Unique-Usernames/1.Unique-Usernames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var names = new HashSet<string>();
+        int linesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < linesCount; i++)
+        {
+            var name = Console.ReadLine();
+            names.Add(name);
+        }
+        Console.WriteLine(string.Join(Environment.NewLine, names));
+    }
+}
+
