2ac8daad6b6c331a96b53dd3386faddd67e74bac
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-26 11:21:35 +0300

    Create 4.Even-Times.cs

---
+++ Exercises-Sets-and-Dictionaries-Advanced/4.Even-Times/4.Even-Times.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var occurences = new Dictionary<int, int>();//number => occurences
+
+        int linesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < linesCount; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (!occurences.ContainsKey(number))
+            {
+                occurences.Add(number, 0);
+            }
+            occurences[number]++;
+        }
+        Console.WriteLine();
+        foreach (var kvp in occurences.Where(x => x.Value % 2 == 0))
+        {
+            Console.WriteLine(kvp.Key);
+        }
+    }
+}
+
