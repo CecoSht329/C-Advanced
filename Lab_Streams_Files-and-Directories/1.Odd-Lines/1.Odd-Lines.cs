f4aff5249c5bcb5849523bbdb08dc6c23b439b16
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-28 10:38:51 +0300

    Create 1.Odd-Lines.cs

---
+++ Lab_Streams_Files-and-Directories/1.Odd-Lines/1.Odd-Lines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace _1.Odd_Lines
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var reader = new StreamReader("Resources/01. Odd Lines/Input.txt"))
+            {
+                int counter = 0;
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+}
