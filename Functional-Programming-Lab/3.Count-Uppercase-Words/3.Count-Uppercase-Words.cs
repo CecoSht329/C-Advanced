5ffe16949ab832f0bec0d7d4f51a1ee92ce9917c
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-29 12:33:51 +0300

    Create 3.Count-Uppercase-Words.cs

---
+++ Functional-Programming-Lab/3.Count-Uppercase-Words/3.Count-Uppercase-Words.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main() => Console.ReadLine()
+       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+       .Where(w => char.IsUpper(w[0]))
+       .ToList()
+       .ForEach(Console.WriteLine);
+
+}
+
