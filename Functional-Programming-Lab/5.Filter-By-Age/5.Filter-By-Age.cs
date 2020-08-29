98ba90d17cf0df0d1c4500ce82e303d28d870e22
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-29 13:29:26 +0300

    Create 5.Filter-By-Age.cs

---
+++ Functional-Programming-Lab/5.Filter-By-Age/5.Filter-By-Age.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+}
+public class Program
+{
+    public static void Main()
+    {
+        int totalPeople = int.Parse(Console.ReadLine());
+
+        var people = new List<Person>();
+
+        for (int i = 0; i < totalPeople; i++)
+        {
+            var currentPerson = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var person = new Person
+            {
+                Name = currentPerson[0],
+                Age = int.Parse(currentPerson[1])
+            };
+            people.Add(person);
+        }
+
+        string condition = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        Func<Person, bool> filterPredicate;
+        if (condition == "older")
+        {
+            filterPredicate = p => p.Age >= age;
+        }
+        else
+        {
+            filterPredicate = p => p.Age < age;
+        }
+
+        string format = Console.ReadLine();
+
+        Func<Person, string> printFunc;
+        if (format == "name age")
+        {
+            printFunc = p => $"{p.Name} - {p.Age}";
+        }
+        else if (format == "name")
+        {
+            printFunc = p => $"{p.Name}";
+        }
+        else
+        {
+            printFunc = p => $"{p.Age}";
+        }
+        people
+            .Where(filterPredicate)
+            .Select(printFunc)
+            .ToList()
+            .ForEach(Console.WriteLine);
+    }
+}
+
