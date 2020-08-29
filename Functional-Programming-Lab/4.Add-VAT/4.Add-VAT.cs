ac1c1b36e6e670fffe3062702fd7e43a1e4a798b
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-29 12:54:51 +0300

    Create 4.Add-VAT.cs

---
+++ Functional-Programming-Lab/4.Add-VAT/4.Add-VAT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var prices = Console.ReadLine()
+            .Split(", ")
+            .Select(decimal.Parse)
+            .Select(x => x + x * 0.2m)
+            .ToList();
+
+        foreach (var price in prices)
+        {
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
+
