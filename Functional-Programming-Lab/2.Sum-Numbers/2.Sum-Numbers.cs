fe283b35e53db84b12e36ad1da625b2b046d1bb9
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-29 08:38:53 +0300

    Create 2.Sum-Numbers.cs

---
+++ Functional-Programming-Lab/2.Sum-Numbers/2.Sum-Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        Console.WriteLine(numbers.Count);
+        Console.WriteLine(numbers.Sum());
+    }
+}
+
