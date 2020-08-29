bc0fa188ca319a7cec5c46a9085afdf06fd21665
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-29 08:34:51 +0300

    Create 1.Sort-Even-Numbers.cs

---
+++ Functional-Programming-Lab/1.Sort-Even-Numbers/1.Sort-Even-Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var numbers = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", numbers));
+    }
+}
+
