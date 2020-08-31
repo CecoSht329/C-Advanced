3754bdab8a4e89db07ea9860b38f4b257006b76d
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 14:03:39 +0300

    Create 7.Predicate-For -Names.cs

---
+++ Functional-Programming-Exercise/7.Predicate-For -Names/7.Predicate-For -Names.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.Predicate_For__Names
+{
+    class Program
+    {
+        static void Main()
+        {
+            var desiredLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= desiredLength)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
