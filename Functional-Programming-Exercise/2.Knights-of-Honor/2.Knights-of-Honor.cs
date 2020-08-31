4dc50c6644ca6240e08131b0686ec6ce3d782960
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 10:25:42 +0300

    Create 2.Knights-of-Honor.cs

---
+++ Functional-Programming-Exercise/2.Knights-of-Honor/2.Knights-of-Honor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Knights_of_Honor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => "Sir " + x)
+            .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
