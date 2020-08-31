1dd0b639f9365db2bfeebf644aae3ecd64f5c26c
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 10:50:45 +0300

    Create 3Custom-Min-Function.cs

---
+++ Functional-Programming-Exercise/3Custom-Min-Function/3Custom-Min-Function.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3Custom_Min_Function
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, int> getSmallest = x => x.Min();
+
+            Console.WriteLine(getSmallest(numbers));
+
+        }
+    }
+}
