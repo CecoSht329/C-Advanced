76ee12e16a4874a46dd1ff3b48fda9b566b215e2
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 14:28:33 +0300

    Create 8.Custom-Comparator.cs

---
+++ Functional-Programming-Exercise/8.Custom-Comparator/8.Custom-Comparator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8.Custom_Comparator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            numbers.Sort();
+            Func<int, bool> filterEven = x => x % 2 == 0;
+            Func<int, bool> filterOdd = x => x % 2 != 0;
+
+            Console.WriteLine($"{string.Join(" ", numbers.Where(filterEven))} {string.Join(" ", numbers.Where(filterOdd))}");
+
+        }
+    }
+}
