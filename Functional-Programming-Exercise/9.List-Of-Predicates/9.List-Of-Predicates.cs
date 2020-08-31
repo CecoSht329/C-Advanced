87e776992c8ee9432b2ee31a2141286c4c6b83fc
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 15:05:41 +0300

    Create 9.List-Of-Predicates.cs

---
+++ Functional-Programming-Exercise/9.List-Of-Predicates/9.List-Of-Predicates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9.List_Of_Predicates
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToList();
+
+            var numberSequense = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (DividersInfo(i, dividers))
+                {
+                    numberSequense.Add(i);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numberSequense));
+        }
+        static bool DividersInfo(int n, List<int> dividers)
+        {
+            bool isTrue = true;
+            foreach (var divider in dividers)
+            {
+                if (n % divider != 0)
+                {
+                    return false;
+                }
+            }
+            return isTrue;
+        }
+    }
+}
