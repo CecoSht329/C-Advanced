19a7f2966a7b4609d747e5e9f822402a961e47f9
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-08-31 13:52:25 +0300

    Create 6.Reverse-And-Exclude.cs

---
+++ Functional-Programming-Exercise/6.Reverse-And-Exclude/6.Reverse-And-Exclude.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            var n = int.Parse(Console.ReadLine());
+            numbers.Reverse();
+            numbers = numbers.Where(x => x % n != 0).ToList();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
