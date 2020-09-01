c40bda23119aa29e2036d30df81977858b287bf5
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-01 15:29:50 +0300

    Create StartUp .cs

---
+++ DefiningClassesLab/CarManufacturer/StartUp .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "Audi";
+            car.Model = "A80";
+            car.Year = 1992;
+            Console.WriteLine($"Make: {car.Make}" +
+                $"{Environment.NewLine}Model: {car.Model}" +
+                $"{Environment.NewLine}Year: {car.Year}");
+        }
+    }
+}
