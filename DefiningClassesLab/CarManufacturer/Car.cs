9c4e0ddb222a2826e166b280f59e4068bc31c1af
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-02 15:58:41 +0300

    Update Car.cs

--- DefiningClassesLab/CarManufacturer/Car.cs
+++ DefiningClassesLab/CarManufacturer/Car.cs
@@ -7,11 +7,61 @@ namespace CarManufacturer
     public class Car
     {
         private string make;
+
+        public string Make
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
         private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
         private int year;
 
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double remainingFuel = FuelQuantity - distance * FuelConsumption;
+            if (remainingFuel > 0)
+            {
+                FuelQuantity -= remainingFuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
     }
 }
