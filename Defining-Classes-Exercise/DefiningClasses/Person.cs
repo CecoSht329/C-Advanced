c22a62d11ba2f45d555b9e3928bfe6af3a994ab2
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-03 10:53:30 +0300

    Update Person.cs

--- Defining-Classes-Exercise/DefiningClasses/Person.cs
+++ Defining-Classes-Exercise/DefiningClasses/Person.cs
@@ -7,6 +7,24 @@ namespace DefiningClasses
     public class Person
     {
         private string name;
+        private int age;
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
 
         public string Name
         {
@@ -14,15 +32,11 @@ namespace DefiningClasses
             set { name = value; }
         }
 
-
-        private int age;
-
         public int Age
         {
             get { return age; }
             set { age = value; }
         }
 
-        
     }
 }
