882f8088ecceac2e254b480cf964b2cc86e3fa24
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-03 09:55:01 +0300

    Create StartUp .cs

---
+++ Defining-Classes-Exercise/DefiningClasses/StartUp .cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person person = new Person();
+
+            person.Name = "Pesho";
+            person.Age = 20;
+
+            Person anotherPerson = new Person();
+            anotherPerson.Name = "Ivan";
+            anotherPerson.Age = 28;
+            Console.WriteLine(person.Name);
+
+        }
+    }
+}
