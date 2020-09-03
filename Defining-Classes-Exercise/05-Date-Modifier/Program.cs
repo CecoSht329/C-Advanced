f3d67e49ebbf9c3022793e7319e7eb7b017513c3
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-03 14:05:17 +0300

    Create Program.cs

---
+++ Defining-Classes-Exercise/05-Date-Modifier/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _05_Date_Modifier
+{
+    class Program
+    {
+        static void Main()
+        {
+            double result = DateModifier.GetDaysBetweeen(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
