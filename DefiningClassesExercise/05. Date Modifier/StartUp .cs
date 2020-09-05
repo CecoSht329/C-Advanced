f223fcc53341b44503d6e70c8a692fc88ada361b
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-05 13:43:31 +0300

    Create StartUp .cs

---
+++ DefiningClassesExercise/05. Date Modifier/StartUp .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DateModifierAssignment
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            double result = DateModifier.CalculateDifference(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
