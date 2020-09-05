1a139b348d7538adb92e6dfa88a6d1febcbe2eac
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-05 15:51:28 +0300

    Create Tire.cs

---
+++ DefiningClassesExercise/07. Raw Data/Tire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Tire
+    {
+        public Tire(double tirePressure, long tireAge)
+        {
+            TirePressure = tirePressure;
+            TireAge = tireAge;
+        }
+
+        //{tirePressure} {tireAge} 
+        public double TirePressure { get; set; }
+        public long TireAge { get; set; }
+    }
+}
