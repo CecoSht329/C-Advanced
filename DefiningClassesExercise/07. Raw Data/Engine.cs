96c6301104354b053fb0f5f0189b66b47fd5860f
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-05 15:51:19 +0300

    Create Engine.cs

---
+++ DefiningClassesExercise/07. Raw Data/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Engine
+    {
+        public Engine(long engineSpeed, long enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+
+        //{engineSpeed} {enginePower} 
+
+        public long EngineSpeed { get; set; }
+        public long EnginePower { get; set; }
+    }
+}
