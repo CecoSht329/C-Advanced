b0ef7c10de79f16fd8aaf6d40d91a5206fa739f9
Tsvetomir Shterev <tsvetomirshterev@gmail.com>
2020-09-05 13:43:26 +0300

    Create DateModifier.cs

---
+++ DefiningClassesExercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace DateModifierAssignment
+{
+    public static class DateModifier
+    {
+
+        public static double CalculateDifference(string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            double result = (dateOne - dateTwo).TotalDays;
+            return result;
+        }
+    }
+}
