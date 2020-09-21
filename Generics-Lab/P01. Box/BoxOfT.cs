4b050dd9321054ef1803addaa6b423b6ef71e2ac
CecoSht329 <62944181+CecoSht329@users.noreply.github.com>
2020-09-21 12:06:26 +0300

    Add files via upload

---
+++ Generics-Lab/P01. Box/BoxOfT.cs
@@ -0,0 +1,40 @@
+﻿namespace BoxOfT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+
+        public Box()
+        {
+            data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Can not remove from empty collection!");
+            }
+            T result = data.Last();
+            data.Remove(result);
+            return result;
+
+        }
+    }
+}
